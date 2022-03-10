77a79e2b65fd1c566d2f1da1f1480c0b376bd7a7
Marcos <marcosvr.lima96@gmail.com>
2022-03-10 20:52:26 -0300

    backup 20:52

--- lib/modulos/login/login3.view.dart
+++ lib/modulos/login/login3.view.dart
@@ -17,36 +17,38 @@ class LoginView3 extends StatelessWidget {
             children: [
               //------------------------------------- ITENS CENTRAIS -------------------------------------
 
-              Container(
-                width: w * 100,
-                height: h * 100,
+              Expanded(
                 child: Scaffold(
                   backgroundColor: Colors.white,
-                  body: Container(
-                    width: w * 100,
-                    height: h * 100,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        ///---------------- LOGO ----------------
-                        ///
-                        // Container(
-                        //   width: w * 35,
-                        //   child: Image.asset("assets/logo1.png"),
-                        // ),
+                  body: SingleChildScrollView(
+                    child: Center(
+                      child: Container(
+                        width: w * 100,
+                        height: h * 100,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            ///---------------- LOGO ----------------
+                            ///
+                            // Container(
+                            //   width: w * 35,
+                            //   child: Image.asset("assets/logo1.png"),
+                            // ),
 
-                        Image.asset("assets/logo1.png"),
+                            Image.asset("assets/logo1.png"),
 
-                        ///------------- FORMULARIO -------------
-                        ///
-                        LoginForm(),
+                            ///------------- FORMULARIO -------------
+                            ///
+                            LoginForm(),
 
-                        ///---------- BOTAO CADASTRAR -----------
+                            ///---------- BOTAO CADASTRAR -----------
 
-                        SizedBox(height: 30),
-                        Text("CADASTRAR!"),
-                      ],
+                            SizedBox(height: 30),
+                            Text("CADASTRAR!"),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 ),
