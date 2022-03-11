c885ee19f1a7eae8f70204003261ee64ef9244c9
Marcos <marcosvr.lima96@gmail.com>
2022-03-11 09:14:53 -0300

    update

--- lib/modulos/login/login.imports.dart
+++ lib/modulos/login/login.imports.dart
@@ -1,9 +1,5 @@
-export 'login.imports.dart'; 
-export 'login.repository.dart'; 
-export 'login.view.dart'; 
-export 'login2.view.dart'; 
-export 'login3.view.dart'; 
-export 'login4.view.dart'; 
-export 'login5.view.dart'; 
-export 'login6.view.dart'; 
-export './components/components.imports.dart'; 
+export 'login.imports.dart';
+export 'login.repository.dart';
+export 'login.view.dart';
+export 'login3.view.dart';
+export './components/components.imports.dart';
