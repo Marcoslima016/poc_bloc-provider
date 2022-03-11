97d6b30b3c7e5f2c10e7f621d49ec5cfc9c513c8
Marcos <marcosvr.lima96@gmail.com>
2022-03-11 11:43:18 -0300

    Desenvolvida tela Home

--- lib/modulos/home/home.view.dart
+++ lib/modulos/home/home.view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../lib.imports.dart';
 import 'home.imports.dart';
 
 class HomeView extends StatelessWidget {
@@ -8,31 +9,51 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (_) => HomeController(),
+      child: WidgetTeste(),
+    );
+  }
+}
+
+class WidgetTeste extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home Page"),
       ),
-      body: BlocProvider(
-        create: (context) => controller..init(),
-        child: Column(
-          children: <Widget>[
-            GestureDetector(
-              onTap: controller.increment,
-              child: Container(
-                width: 100,
-                height: 40,
-                color: Colors.blue,
-                child: Text(
-                  "Testar!",
-                ),
+      body: Column(
+        children: <Widget>[
+          //
+          GestureDetector(
+            onTap: () {
+              // controller.getUserData();
+              var teste = context.read<HomeController>();
+              var point = "";
+              context.read<HomeController>().getUserData();
+            },
+            child: Container(
+              width: 100,
+              height: 40,
+              color: Colors.blue,
+              child: Text(
+                "Testar!",
               ),
             ),
-            Text("Valor atual: "),
-            BlocBuilder<HomeController, HomeState>(
-              builder: (context, HomeState state) => Center(child: Text(state.count.toString())),
-            ),
-          ],
-        ),
+          ),
+
+          Text("Valor atual: "),
+          BlocBuilder<HomeController, User>(
+            builder: (context, User state) {
+              return Center(
+                child: Text(
+                  state.nome.toString(),
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
